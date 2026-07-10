1db8cc1e0842d4e31858368c1e327202d29cc6cf
Karan Deshmukh <karandeshmukh2509@gmail.com>
2026-07-10 16:13:09 +0530

    Time: 0 ms (100%), Space: 53 MB (38.68%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int singleNonDuplicate(int[] nums) {
         int xor=0;
         for(int i=0;i<nums.length;i++){
@@ -6,5 +6,40 @@ class Solution {
         }
         return xor;
 
+    }
+}*/
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        
+        if (j == 0) {
+            return nums[0];
+        }
+        
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            
+            boolean leftCheck = (mid == 0 || nums[mid] != nums[mid - 1]);
+            boolean rightCheck = (mid == nums.length - 1 || nums[mid] != nums[mid + 1]);
+            
+            if (leftCheck && rightCheck) {
+                return nums[mid];
+            }
+            
+            // Determine which half to eliminate based on index parity
+            // If mid is even, its partner should be at mid + 1
+            // If mid is odd, its partner should be at mid - 1
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || 
+                (mid % 2 != 0 && nums[mid] == nums[mid - 1])) {
+                // We are in the left half before the single element -> search right
+                i = mid + 1;
+            } else {
+                // We are in the right half after the single element -> search left
+                j = mid - 1;
+            }
+        }
+        
+        return -1;
     }
 }
